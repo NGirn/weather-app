32b0c573be719d771d80267abd3828ff49dca91c
Nav <navdeepsinghgirn@gmail.com>
2019-06-16 18:16:52 +0100

    You can click on the temperature and it will alternate between F and C

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 window.addEventListener("load", () => {
   var long;
   var lat;
-  var temperatureDescription = document.querySelector('.temperature-description');
-  var temperatureDegree = document.querySelector('.temperature-degree');
-  var locationTimezone = document.querySelector('.location-timezone');
+  var temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  var temperatureDegree = document.querySelector(".temperature-degree");
+  var locationTimezone = document.querySelector(".location-timezone");
+  var temperatureSection = document.querySelector(".temperature");
+  var temperatureSpan = document.querySelector('.temperature span');
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const proxy = `https://cors-anywhere.herokuapp.com/`
+      const proxy = `https://cors-anywhere.herokuapp.com/`;
       const api = `${proxy}https://api.darksky.net/forecast/0840fdbdc19596154dc23613a42694a8/${lat},${long}`;
 
       fetch(api)
@@ -18,19 +22,29 @@ window.addEventListener("load", () => {
           return response.json();
         })
         .then(data => {
-          const {temperature, summary, icon} = data.currently;
+          const { temperature, summary, icon } = data.currently;
           temperatureDegree.textContent = temperature;
           temperatureDescription.textContent = summary;
           locationTimezone.textContent = data.timezone;
-          setIcons(icon, document.querySelector('.icon'));
-          
+          var celsius = (temperature - 32) * (5 / 9);
+          setIcons(icon, document.querySelector(".icon"));
+
+          temperatureSection.addEventListener('click', () => {
+              if (temperatureSpan.textContent === 'F') {
+                  temperatureSpan.textContent = 'C';
+                  temperatureDegree.textContent = Math.floor(celsius);
+              } else {
+                  temperatureSpan.textContent = 'F';
+                  temperatureDegree.textContent = temperature;
+              }
+          })
         });
     });
   }
-  function setIcons(icon, iconID){
-      const skycons = new Skycons({color: 'white'});
-      const currentIcon = icon.replace(/-/g, '_').toUpperCase();
-      skycons.play();
-      return skycons.set(iconID, Skycons[currentIcon]);
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
   }
 });
